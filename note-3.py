49ca53f0a593427ada20f8392140189b1d6616e6
RonitBakshi <76399351+RonitBakshi@users.noreply.github.com>
2023-12-18 16:46:45 +0530

    Update note-3.py

--- note-3.py
+++ note-3.py
@@ -1,135 +1,208 @@
 import cv2
 import mediapipe as mp
- 
-WRIST = 0
-T1 = 1
-T2 = 2
-T3 = 3
-T4 = 4
-IF1 = 5
-IF2 = 6
-IF3 = 7
-IF4 = 8
-MF1 = 9
-MF2 = 10
-MF3 = 11
-MF4 = 12
-RF1 = 13
-RF2 = 14
-RF3 = 15
-RF4 = 16
-PF1 = 17
-PF2 = 18
-PF3 = 19
-PF4 = 20
-# ctrl + c : Stop execution 
-# ctrl + l : Clear Terminal
+import tkinter as tk
+from PIL import Image, ImageTk
 
-cap = cv2.VideoCapture(0)
+# Landmark indices
+WRIST, T1, T2, T3, T4, IF1, IF2, IF3, IF4, MF1, MF2, MF3, MF4, RF1, RF2, RF3, RF4, PF1, PF2, PF3, PF4 = range(21)
 
-mpHands = mp.solutions.hands
-hands = mpHands.Hands()
-mpDraw = mp.solutions.drawing_utils
+# Global variables
+is_recognition_started = False
+opening_root = None
+recognition_root = None
 
-while True:
-    success , img = cap.read()
-    imgRGB = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-    results = hands.process(imgRGB)
+# Color Palette
+bg_color = "#392467"
+text_color = "#FFD1E3"
+subtext_color = "#A367B1"
+button_color = "#5D3587"
 
-    lmList = []
+def start_recognition():
+    global is_recognition_started, opening_root, recognition_root
+    is_recognition_started = True
+    opening_position = opening_root.geometry()  # Get the position of the opening screen
+    opening_root.destroy()
 
-    if results.multi_hand_landmarks:
-        myHands = results.multi_hand_landmarks[0]
+    # Hand Gesture Recognition
+    cap = cv2.VideoCapture(0)
+    mpHands = mp.solutions.hands
+    hands = mpHands.Hands()
+    mpDraw = mp.solutions.drawing_utils
 
-        mpDraw.draw_landmarks(img, myHands, mpHands.HAND_CONNECTIONS)
+    recognition_root = tk.Tk()
+    recognition_root.title("Hand Gesture Recognition")
+    recognition_root.geometry(opening_position)  # Set the dimensions and position of the recognition screen
 
-        for id,lm in enumerate(myHands.landmark):
-            h, w, c = img.shape
-            cx, cy = int(lm.x*w), int(lm.y*h)
-            lmList.append([id,cx,cy])
+    # Apply the color palette
+    recognition_root.configure(bg=bg_color)
 
-        text = ''
+    # Create a frame for the footer
+    footer_frame = tk.Frame(recognition_root)
+    footer_frame.pack(side=tk.BOTTOM, pady=10)
 
-        if lmList[PF1][1]-lmList[WRIST][1]:
+    # Create a label for displaying text in the footer
+    footer_label = tk.Label(footer_frame, text="No Sign Detected", font=("Impact", 30), bg=bg_color, fg=text_color, pady=10)
+    footer_label.pack(fill=tk.X)
 
-            tan = (lmList[PF1][2]-lmList[WRIST][2])/(lmList[PF1][1]-lmList[WRIST][1])
+    label = tk.Label(footer_frame, text="", font=("Helvetica", 20), bg=bg_color)
+    label.pack()
 
-            if  -0.9 < tan < 0.9: # Hand is Sideways
-                IFU = lmList[IF2][1] > lmList[IF4][1]
-                MFU = lmList[MF2][1] > lmList[MF4][1]
-                RFU = lmList[RF2][1] > lmList[RF4][1]
-                PFU = lmList[PF2][1] > lmList[PF4][1]
+    def update_label_text():
+        """
+        Update the label text with the recognized gesture.
+        """
+        try:
+            success, img = cap.read()
+            if not success:
+                raise ValueError("Failed to capture video frame")
 
-                if RFU and PFU:
-                    if lmList[T4][2]-lmList[IF4][2] > 40:
-                        text = 'C'
-                    elif lmList[T4][2]-lmList[IF4][2] < 25:
-                        text = 'O'
-                else:
-                    if MFU and IFU:
-                        if lmList[MF4][2] - lmList[IF2][2] > 15:
-                            text = 'P'
+            imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            results = hands.process(imgRGB)
+
+            lmList = []
+            text = ""
+
+            if results.multi_hand_landmarks:
+                myHands = results.multi_hand_landmarks[0]
+                mpDraw.draw_landmarks(img, myHands, mpHands.HAND_CONNECTIONS)
+
+                for id, lm in enumerate(myHands.landmark):
+                    h, w, c = img.shape
+                    cx, cy = int(lm.x * w), int(lm.y * h)
+                    lmList.append([id, cx, cy])
+
+                # Your gesture recognition logic goes here
+                if lmList[PF1][1]-lmList[WRIST][1]:
+
+                    tan = (lmList[PF1][2]-lmList[WRIST][2])/(lmList[PF1][1]-lmList[WRIST][1])
+
+                    if  -0.9 < tan < 0.9: # Hand is Sideways
+                        IFU = lmList[IF2][1] > lmList[IF4][1]
+                        MFU = lmList[MF2][1] > lmList[MF4][1]
+                        RFU = lmList[RF2][1] > lmList[RF4][1]
+                        PFU = lmList[PF2][1] > lmList[PF4][1]
+
+                        if RFU and PFU:
+                            if lmList[T4][2]-lmList[IF4][2] > 40:
+                                text = 'C'
+                            elif lmList[T4][2]-lmList[IF4][2] < 25:
+                                text = 'O'
                         else:
-                            text = 'H'
-                    elif not MFU and IFU: 
-                        if lmList[WRIST][2] < lmList[T1][2]:  
-                            text = 'Q'
-                        else: 
-                            text = 'G'
-            else: # Straight Hand
-                IFU = lmList[IF1][2] > lmList[IF4][2]
-                MFU = lmList[MF1][2] > lmList[MF4][2]
-                RFU = lmList[RF1][2] > lmList[RF4][2]
-                PFU = lmList[PF1][2] > lmList[PF4][2]
-                TU = lmList[T4][1] > lmList[T2][1]
+                            if MFU and IFU:
+                                if lmList[MF4][2] - lmList[IF2][2] > 15:
+                                    text = 'P'
+                                else:
+                                    text = 'H'
+                            elif not MFU and IFU: 
+                                if lmList[WRIST][2] < lmList[T1][2]:  
+                                    text = 'Q'
+                                else: 
+                                    text = 'G'
+                    else: # Straight Hand
+                        IFU = lmList[IF1][2] > lmList[IF4][2]
+                        MFU = lmList[MF1][2] > lmList[MF4][2]
+                        RFU = lmList[RF1][2] > lmList[RF4][2]
+                        PFU = lmList[PF1][2] > lmList[PF4][2]
+                        TU = lmList[T4][1] > lmList[T2][1]
 
-                if not PFU and not RFU and not MFU and not IFU and TU:
-                    text = 'A'
-                elif PFU and RFU and MFU and IFU and not TU:
-                    text = 'B'
-                elif not PFU and not RFU and not MFU and IFU and not TU:
-                    if lmList[IF4][2] - lmList[IF3][2] > 0:
-                        text = 'X'
-                    else:
-                        text = 'D'
-                elif not PFU and not RFU and not MFU and not IFU and not TU:
-                    if lmList[T4][2] > lmList[IF4][2]:
-                        text = 'E'
-                    elif lmList[T4][2] < lmList[IF2][2]:
-                        if lmList[T3][1] < lmList[IF3][1] and lmList[T4][1] > lmList[MF3][1]:
-                            text = 'T'
-                        elif lmList[T4][1] < lmList[MF4][1]:
-                            text = 'N'
-                    elif lmList[T4][1] < lmList[MF2][1]:
-                        text = 'S'
-                    else:
-                        text = 'M'
-                elif PFU and RFU and MFU and not IFU and not TU:
-                    text = 'F'
-                elif PFU and not RFU and not MFU and not IFU and not TU:
-                    text = 'I'
-                elif not PFU and not RFU and MFU and IFU:
-                    if lmList[MF4][1] > lmList[IF4][1]:
-                        text = 'R'
-                    elif lmList[T4][1] > lmList[MF1][1]:
-                        text = 'K'
-                    elif lmList[IF4][1] - lmList[MF4][1] > 24:
-                        text = 'V'
-                    else:
-                        text = 'U'
-                elif not PFU and not RFU and not MFU and IFU and TU:
-                    text = 'L'
-                elif not PFU and RFU and MFU and IFU and not TU:
-                    text = 'W'
-                elif PFU and not RFU and not MFU and not IFU and TU:
-                    text = 'Y'
+                        if not PFU and not RFU and not MFU and not IFU and TU:
+                            text = 'A'
+                        elif PFU and RFU and MFU and IFU and not TU:
+                            text = 'B'
+                        elif not PFU and not RFU and not MFU and IFU and not TU:
+                            if lmList[IF4][2] - lmList[IF3][2] > 0:
+                                text = 'X'
+                            else:
+                                text = 'D'
+                        elif not PFU and not RFU and not MFU and not IFU and not TU:
+                            if lmList[T4][2] > lmList[IF4][2]:
+                                text = 'E'
+                            elif lmList[T4][2] < lmList[IF2][2]:
+                                if lmList[T3][1] < lmList[IF3][1] and lmList[T4][1] > lmList[MF3][1]:
+                                    text = 'T'
+                                elif lmList[T4][1] < lmList[MF4][1]:
+                                    text = 'N'
+                            elif lmList[T4][1] < lmList[MF2][1]:
+                                text = 'S'
+                            else:
+                                text = 'M'
+                        elif PFU and RFU and MFU and not IFU and not TU:
+                            text = 'F'
+                        elif PFU and not RFU and not MFU and not IFU and not TU:
+                            text = 'I'
+                        elif not PFU and not RFU and MFU and IFU:
+                            if lmList[MF4][1] > lmList[IF4][1]:
+                                text = 'R'
+                            elif lmList[T4][1] > lmList[MF1][1]:
+                                text = 'K'
+                            elif lmList[IF4][1] - lmList[MF4][1] > 24:
+                                text = 'V'
+                            else:
+                                text = 'U'
+                        elif not PFU and not RFU and not MFU and IFU and TU:
+                            text = 'L'
+                        elif not PFU and RFU and MFU and IFU and not TU:
+                            text = 'W'
+                        elif PFU and not RFU and not MFU and not IFU and TU:
+                            text = 'Y'
 
-        cv2.putText(img,text,(100,100),cv2.FONT_HERSHEY_PLAIN, 3, (0,255,255), 5)
+                if text:
+                    footer_label.config(text="Sign Detected: {}".format(text))
+                else:
+                    footer_label.config(text="No Sign Detected")
+                
+                label.config(text=text)
+            else:
+                footer_label.config(text="Please raise your hand")
+                label.config(text="")
 
-    cv2.imshow("Image",img)
+            # Display the video feed
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            img = Image.fromarray(img)
+            img = ImageTk.PhotoImage(image=img)
+            label.img = img
+            label.configure(image=img)
 
-    if cv2.waitKey(1)==ord('q'):
-        break
+        except Exception as e:
+            print(f"Error: {e}")
 
-cap.release()
-cv2.destroyAllWindows()
+        finally:
+            # Schedule the next update after 10 milliseconds
+            recognition_root.after(10, update_label_text)
+
+    # Schedule the first update
+    update_label_text()
+    recognition_root.mainloop()
+
+    # Release the camera when the application is closed
+    cap.release()
+    cv2.destroyAllWindows()
+
+# Opening Page
+opening_root = tk.Tk()
+opening_root.title("ASL Classification")
+opening_root.geometry("600x500")  # Set the dimensions of the opening screen
+
+# Apply the color palette
+opening_root.configure(bg=bg_color)
+
+opening_label = tk.Label(opening_root, text="ASL Classification\nusing Deep Learning", font=("Impact", 40), pady=20, bg=bg_color, fg=text_color)
+opening_label.pack()
+
+start_button = tk.Button(opening_root, text="Start", font=("Impact", 20), command=start_recognition, bg=button_color, fg=text_color)
+start_button.pack()
+
+# Text below the start button
+below_start_button_text = tk.Label(opening_root, text="""
+Submitted By
+RONIT BAKSHI\t\t(11115602720)
+SATWIK PANDEY\t\t(11615602720)
+TANMAY PARNAMI\t\t(13115602720)
+UTKARSH JAIN\t\t(13615602720)
+                                   
+Under the guidance of
+Mr. Lokesh Meena,
+Assistant Professor, CSE Department""", font=("Helvetica", 12), bg=bg_color, fg=subtext_color, pady=20)
+below_start_button_text.pack()
+
+opening_root.mainloop()
